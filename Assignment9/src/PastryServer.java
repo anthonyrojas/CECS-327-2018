619e4efb52e2e2e7debaec0a1f0428a02f273357
anthonyrojas <anthony.rojas095@gmail.com>
2018-04-20 14:45:16 -0700

    modifying files

--- Assignment9/src/PastryServer.java
+++ Assignment9/src/PastryServer.java
@@ -2,34 +2,34 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 public class PastryServer {
-    private static Map<Integer, String> leafSet;
-    private static Map<Integer, String> routingTable;
+    private static Map<String, String> leafSet;
+    private static Map<String, String> routingTable;
     private final static int MY_PASTRY = 1201;
     private final static String MY_IP = "54.183.205.102";
     public static void main(String [] args){
         leafSet = new TreeMap<>();
-        leafSet.put(1131, "52.10.42.73");
-        leafSet.put(1200, "18.188.49.131");
-        leafSet.put(1220, "52.53.151.57");
-        leafSet.put(1300, "18.219.11.198");
+        leafSet.put("1131", "52.10.42.73");
+        leafSet.put("1200", "18.188.49.131");
+        leafSet.put("1220", "52.53.151.57");
+        leafSet.put("1300", "18.219.11.198");
         routingTable = new TreeMap<>();
-        routingTable.put(0001, "54.67.23.56");
-        routingTable.put(1201, "54.183.205.102");
-        routingTable.put(2020, "18.219.20.177");
-        routingTable.put(3001, "13.57.5.2");
-        routingTable.put(1013, "18.144.32.67");
-        routingTable.put(1100, "18.217.157.190");
-        routingTable.put(1300, "18.219.11.198");
-        routingTable.put(121, "NULL");
-        routingTable.put(1220, "52.53.157.57");
-        routingTable.put(123, "NULL");
-        routingTable.put(1200, "18.188.49.131");
-        routingTable.put(1202, "NULL");
-        routingTable.put(1203, "NULL");
+        routingTable.put("0001", "54.67.23.56");
+        routingTable.put("1201", "54.183.205.102");
+        routingTable.put("2020", "18.219.20.177");
+        routingTable.put("3001", "13.57.5.2");
+        routingTable.put("1013", "18.144.32.67");
+        routingTable.put("1100", "18.217.157.190");
+        routingTable.put("1300", "18.219.11.198");
+        routingTable.put("121", "NULL");
+        routingTable.put("1220", "52.53.157.57");
+        routingTable.put("123", "NULL");
+        routingTable.put("1200", "18.188.49.131");
+        routingTable.put("1202", "NULL");
+        routingTable.put("1203", "NULL");
         DatagramSocket aSocket = null;
         try{
             aSocket = new DatagramSocket(32710);
-            byte[] buffer = new byte[100];
+            byte[] buffer = new byte[1000];
             while (true){
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
@@ -50,7 +50,7 @@ public class PastryServer {
         }
     }
     public static String getPastry(String nodeStr){
-        String reply = "NULL";
+        String reply;
         switch (nodeStr.length()){
             case 0: reply = "NULL";
                 break;
@@ -72,17 +72,17 @@ public class PastryServer {
     }
 
     public static String getPastryReply(String nodeStr, int length){
-        if(String.valueOf(MY_IP).startsWith(nodeStr)){
-            return String.valueOf(MY_IP) + ":" + MY_IP ;
+        if(String.valueOf(MY_PASTRY).startsWith(nodeStr)){
+            return String.valueOf(MY_PASTRY) + ":" + MY_IP ;
         }else{
-            for(int k : leafSet.keySet()){
-                if(String.valueOf(k).startsWith(nodeStr)){
-                    return String.valueOf(k) + ":" + leafSet.get(k);
+            for(String k : leafSet.keySet()){
+                if(k.startsWith(nodeStr)){
+                    return k + ":" + leafSet.get(k);
                 }
             }
-            for(int k : routingTable.keySet()){
-                if(String.valueOf(k).startsWith(nodeStr)){
-                    return String.valueOf(k) + ":" + routingTable.get(k);
+            for(String k : routingTable.keySet()){
+                if(k.startsWith(nodeStr)){
+                    return k + ":" + routingTable.get(k);
                 }
             }
         }
